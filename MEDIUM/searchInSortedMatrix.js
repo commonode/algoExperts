443310cb57a18102ea396d03d0b180734492fde5
andrewlidong <andrewdong1994@gmail.com>
2019-03-11 16:19:19 -0500

    Updated search in sorted matrix

--- MEDIUM/searchInSortedMatrix.js
+++ MEDIUM/searchInSortedMatrix.js
@@ -1,3 +1,5 @@
 Search in Sorted Matrix
 
 You are given a two-dimensional array (matrix) of distinct integers where each row is sorted and each column is also sorted.  The matrix does not necessarily have the same height and width.  You are also given a target number, and you must write a function that returns an array of the row and column indices of the target number if it is contained in the matrix and [-,1-1] if it is not contained in the matrix.  
+
+Pick any number in the matrix and compare it to the target number.  If the number is bigger than the target number, what does this tell you about all the other numbers row and this columns number.  Try starting at the top right corner of the matrix, comparing the number there to the target number, and using whatever you gathered to figure out what number to compare next if the top right number isn't equal to the target number.  
