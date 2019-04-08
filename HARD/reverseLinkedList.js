8c6b207f6d2c7c643453d7700a871719a81eaac2
andrewlidong <andrewdong1994@gmail.com>
2019-04-08 16:19:19 -0500

    reverse linked list added

--- HARD/reverseLinkedList.js
+++ HARD/reverseLinkedList.js
@@ -1,3 +1,16 @@
-Reverse Linked List 
+// Reverse Linked List 
 
-Write a function that takes in the head of a Singly Linked List (assume that the list has at leaset 1 Node; in other words, the head will never be a null value).  The function should reverse the list and return its new head.  Note that every node in the Singly Linked List has a "value" property storings its value as well as a "next" property pointing to the next node in the list or None(null) if it is the tail of the list.  
+// Write a function that takes in the head of a Singly Linked List (assume that the list has at leaset 1 Node; in other words, the head will never be a null value).  The function should reverse the list and return its new head.  Note that every node in the Singly Linked List has a "value" property storings its value as well as a "next" property pointing to the next node in the list or None(null) if it is the tail of the list.  
+
+// O(n) time | O(1) space - where n is the number of nodes in the Linked List
+function reverseLinkedList(head) {
+    let p1 = null,
+        p2 = head;
+    while (p2 !== null) {
+        const p3 = p2.next;
+        p2.next = p1;
+        p1 = p2;
+        p2 = p3;
+    }
+    return p1;
+}
