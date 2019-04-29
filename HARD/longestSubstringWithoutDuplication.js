ea291e3b50aa4c8b22a534569d14812eda873556
andrewlidong <andrewdong1994@gmail.com>
2019-04-29 16:19:19 -0500

    Longest substring without duplication added

--- HARD/longestSubstringWithoutDuplication.js
+++ HARD/longestSubstringWithoutDuplication.js
@@ -1,1 +1,3 @@
 Longest substring without Duplication
+
+Write a function that takes in a string and that returns its longest substring without duplicate characters.  Assume that there will only be one longest substring without duplication.  
