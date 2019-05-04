7a3dcb7d92ffc766d7280508f2dedb54e286b459
andrewlidong <andrewdong1994@gmail.com>
2019-05-04 16:19:19 -0500

    Airport connections added

--- EXTREMELY HARD/airportConnections.js
+++ EXTREMELY HARD/airportConnections.js
@@ -1,1 +1,3 @@
 Airport Connections
+
+You are given a list of airports (three-letter codes like 'JFK'), a list of routes (one way flights from one airport to another) and a starting airport.  Write a function that returns the minimum number of airport connections (one-way flights) that need to be added in order for someone to be able to reach any airport in the list, starting at the starting airport.  Note that the connections don't have to be direct; it's okay if an airport can only be reached from the starting airport by stopping at other airports first.  
