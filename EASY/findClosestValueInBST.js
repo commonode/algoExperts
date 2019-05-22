683797d352ce7aa0c2ca664fc420294d94a3231b
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-22 13:42:05 -0400

    Find closest value in BST

--- EASY/findClosestValueInBST.js
+++ EASY/findClosestValueInBST.js
@@ -9,3 +9,46 @@
 // Each BST node has an integer value stored in a property called "value" and two children nodes stored in properties called "left" and "right" respectively.
 
 // There will only be one closest value.  
+
+// Average: O(log(n)) time | O(log(n)) space
+// Worst: O(n) time | O(n) space
+function findClosestValueInBst(tree, target) {
+    return findClosestValueInBstHelper(tree, target, Infinity);
+}
+
+function findClosestValueInBstHelper(tree, target, closest) {
+    if (tree === null) return closest;
+    if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+        closest = tree.value;
+    }
+    if (target < tree.value) {
+        return findClosestValueInBstHelper(tree.left, target, closest);
+    } else if (target > tree.value) {
+        return findClosestValueInBstHelper(tree.right, target, closest);
+    } else {
+        return closest;
+    }
+}
+
+// Average: O(log(n)) time | O(1) space
+// Worst: O(n) time | O(1) space
+function findClosestValueInBst(tree, target) {
+    return findClosestValueInBstHelper(tree, target, Infinity);
+}
+
+function findClosestValueInBstHelper(tree, target, closest) {
+    let currentNode = tree;
+    while (currentNode !== null) {
+        if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+            closest = currentNode.value;
+        }
+        if (target < currentNode.value) {
+            currentNode = currentNode.left;
+        } else if (target > currentNode.value) {
+            currentNode = currentNode.right;
+        } else {
+            break;
+        }
+    }
+    return closest;
+}
