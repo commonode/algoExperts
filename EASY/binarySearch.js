00b75675bb0f80ffb3ac808486d15217adfabed5
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-22 15:29:00 -0400

    Binary Search Finished

--- EASY/binarySearch.js
+++ EASY/binarySearch.js
@@ -3,3 +3,39 @@
 // Write a function that takes in a sorted array of integers as well as a target integer.  
 
 // The function should use the Binary Search algorthm to find if the target number is contained in the array and should return its index if it is, otherwise -1.  
+
+function binarySearch(array, target) {
+    return binarySearchHelper(array, target, 0, array.length - 1);
+}
+
+function binarySearchHelper(array, target, left, right) {
+    if (left > right) return -1;
+    const middle = Math.floor((left + right) / 2);
+    const potentialMatch = array[middle];
+    if (target === potentialMatch) {
+        return middle;
+    } else if (target < potentialMatch) {
+        return binarySearchHelper(array, target, left, middle - 1);
+    } else {
+        return binarySearchHelper(array, target, middle + 1, right);
+    }
+}
+
+function binarySearch(array, target) {
+    return binarySearchHelper(array, target, 0, array.length - 1);
+}
+
+function binarySearchHelper(array, target, left, right) {
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2);
+        const potentialMatch = array[middle];
+        if (target === potentialMatch) {
+            return middle;
+        } else if (target < potentialMatch) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+    return -1;
+}
