1c16a3febfaba80078bc357d0f7a1bbb5bebc4b0
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-22 13:29:29 -0400

    Two Number Sum Finished

--- EASY/twoNumberSum.js
+++ EASY/twoNumberSum.js
@@ -7,3 +7,49 @@
 // If no two numbers sum up to the target sum, the function should return an empty array.  
 
 // Assume that there will be at most one pair of numbers summing up to the target sum.  
+
+// O(n^2) time | O(1) space
+function twoNumberSum(array, targetSum) {
+    for (let i = 0; i < array.length - 1; i++) {
+        const firstNum = array[i];
+        for (let j = i + 1; j < array.length; j++) {
+            const secondNum = array[j];
+            if (firstNum + secondNum === targetSum) {
+                return [firstNum, secondNum].sort((a,b) => a - b);
+            }
+        }
+    }
+    return [];
+}
+
+// O(n) time | O(n) space
+function twoNumberSum(array, targetSum) {
+    const nums = {};
+    for (const num of array) {
+        const potentialMatch = targetSum - num;
+        if (potentialMatch in nums) {
+            return [potentialMatch, num].sort((a,b) => a - b);
+        } else {
+            nums[num] = true;
+        }
+    }
+    return [];
+}
+
+// O(nlog(n)) time | O(1) space
+function twoNumberSum(array, targetSum) {
+    array.sort((a,b) => a - b);
+    let left = 0;
+    let right = array.length - 1;
+    while (left < right) {
+        const currentSum = array[left] + array[right];
+        if (currentSum === targetSum) {
+            return [array[left], array[right]];
+        } else if (currentSum < targetSum) {
+            left++;
+        } else if (currentSum > targetSum) {
+            right--;
+        }
+    }
+    return [];
+}
