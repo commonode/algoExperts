63265a948b055049c7618affbf1ea55897e547e3
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-23 10:03:32 -0400

    Insertion sort finished

--- EASY/insertionSort.js
+++ EASY/insertionSort.js
@@ -3,3 +3,21 @@
 // Write a function that takes in an array of integers and returns a sorted version of that array.  
 
 // Use the insertion sort algorithm to sort the array
+
+// O(n^2) time | O(1) space
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let j = i;
+        while (j > 0 && array[j] < array[j - 1]) {
+            swap(j, j-1, array);
+            j -= 1;
+        }
+    }
+    return array;
+}
+
+function swap(i,j,array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
