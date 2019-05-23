fcb28bc20e347e8f38c322be5698a79b33dad435
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-22 21:24:18 -0400

    Three Largest Numbers Finished

--- EASY/threeLargestNumbers.js
+++ EASY/threeLargestNumbers.js
@@ -3,3 +3,32 @@
 // Write a function that takes in an array of integers and returns a sorted array of the three largest integers in the input array.  
 
 // Note that the function should return duplicate integers if ncessary.  
+
+// O(n) time | O(1) space
+function findThreeLargestNumbers(array) {
+    const threeLargest = [null, null, null];
+    for (const num of array) {
+        updateLargest(threeLargest, num);
+    }
+    return threeLargest;
+}
+
+function updateLargest(threeLargest, num) {
+    if (threeLargest[2] === null || num > threeLargest[2]) {
+        shiftAndUpdate(threeLargest, num, 2);
+    } else if (threeLargest[1] === null || num > threeLargest[1]) {
+        shiftAndUpdate(threeLargest, num, 1);
+    } else if (threeLargest[0] === null || num > threeLargest[0]) {
+        shiftAndUpdate(threeLargest, num, 0);
+    }
+}
+
+function shiftAndUpdate(array, num, idx) {
+    for (let i = 0; i <= idx; i++) {
+        if (i === idx) {
+            array[i] = num;
+        } else {
+            array[i] = array[i + 1];
+        }
+    }
+}
