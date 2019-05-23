f045a2fd581366d7e336d003682bff6cb2760aaa
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-23 10:22:59 -0400

    Caesar Cipher Encryptor finished

--- EASY/caesarCipherEncryptor.js
+++ EASY/caesarCipherEncryptor.js
@@ -4,3 +4,34 @@
 // Note that letters should "wrap" around the alphabet.
 
 // In other words, the letter "z" shifted by 1 returns the letter "a"
+
+// O(n) time | O(n) space
+function caesarCipherEncryptor(string, key) {
+    const newLetters = [];
+    const newKey = key % 26;
+    for (const letter of string) {
+        newLetters.push(getNewLetter(letter, newKey));
+    }
+    return newLetters.join("");
+}
+
+function getNewLetter(letter, key) {
+    const newLetterCode = letter.charCodeAt() + key;
+    return newLetterCode <= 122 ? String.fromCharCode(newLetterCode) : String.fromCharCode(96 + newLetterCode % 122);
+}
+
+// O(n) time | O(n) space
+function caesarCipherEncryptor(string, key) {
+    const newLetters = [];
+    const newKey = key % 26;
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    for (const letter of string) {
+        newLetters.push(getNewLetter(letter, newKey, alphabet));
+    }
+    return newLetters.join("");
+}
+
+function getNewLetter(letter, key, alphabet) {
+    const newLetterCode = alphabet.indexOf(letter) + key;
+    return newLetterCode <= 25 ? alphabet[newLetterCode] : alphabet[-1 + newLetterCode % 25];
+}
--- EASY/palindromeCheck.js
+++ EASY/palindromeCheck.js
@@ -7,7 +7,7 @@
 // O(n^2) time | O(n) space
 function isPalindrome(string) {
     let reversedString = "";
-    for (let i = string.length - 1; i >= 0; i==) {
+    for (let i = string.length - 1; i >= 0; i--) {
         reversedString += string[i];
     }
     return string === reversedString;
