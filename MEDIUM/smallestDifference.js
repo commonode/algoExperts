e2b9498a95d29901badc9a89f067d603e57d0c07
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-23 12:39:31 -0400

    Finished smallest difference

--- MEDIUM/smallestDifference.js
+++ MEDIUM/smallestDifference.js
@@ -1,1 +1,36 @@
-Smallest Difference
+// Smallest Difference
+
+// Write a function that takes in two non - empty arrays of integers.
+
+// The function should find the pair of numbers(one from first, one from second) whose absolute difference is closest to zero.The function should return an array containing these two numbers, with the number from the first array in the first position.  
+
+// Assume there will only be one pair of numbers with the smallest difference.  
+
+// O(nlog(n) + mlog(m)) time | O(1) space
+function smallestDifference(arrayOne, arrayTwo) {
+    arrayOne.sort((a,b) => a - b);
+    arrayTwo.sort((a,b) => a - b);
+    let idxOne = 0;
+    let idxTwo = 0;
+    let smallest = Infinity;
+    let current = Infinity;
+    let smallestPair = [];
+    while (idxOne < arrayOne.length && idxTwo < arrayTwo.length) {
+        let firstNum = arrayOne[idxOne];
+        let secondNum = arrayTwo[idxTwo];
+        if (firstNum < secondNum) {
+            current = secondNum - firstNum;
+            idxOne++;
+        } else if (secondNum < firstNum) {
+            current = firstNum - secondNum;
+            idxTwo++;
+        } else {
+            return [firstNum, secondNum];
+        }
+        if (smallest > current) {
+            smallest = current;
+            smallestPair = [firstNum, secondNum];
+        }
+    }
+    return smallestPair;
+}
