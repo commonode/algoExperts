3232dc5bf7f03608df5efa3801efec69a8b56d3d
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-23 10:09:54 -0400

    Selection sort finished

--- EASY/selectionSort.js
+++ EASY/selectionSort.js
@@ -3,3 +3,23 @@
 // Write a function that takes in an array of integers and returns a sorted version of that array.  
 
 // Use the selection sort algorithm to sort the array
+
+// O(n^2) time | O(1) space
+function selectionSort(array) {
+    let startIdx = 0;
+    while (startIdx < array.length - 1) {
+        let smallestIdx = startIdx;
+        for (let i = startIdx + 1; i < array.length; i++) {
+            if (array[smallestIdx] > array[i]) smallestIdx = i;
+        }
+        swap(startIdx, smallestIdx, array);
+        startIdx++;
+    }
+    return array;
+}
+
+function swap(i,j,array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
