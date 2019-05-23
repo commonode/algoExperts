49cc7cdbea0e6558e737c973fb24b24fc86b4461
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-22 21:43:49 -0400

    Bubble sort finished

--- EASY/bubbleSort.js
+++ EASY/bubbleSort.js
@@ -3,3 +3,20 @@
 // Write a function that takes in an array of integers and returns a sorted version of that array.  
 
 // Use the bubble sort algorithm to sort the array
+
+// O(n^2) time | O(1) space
+function bubbleSort(array) {
+    let isSorted = false;
+    let counter = 0;
+    while (!isSorted) {
+        isSorted = true;
+        for (let i = 0; i < array.length - 1 - counter; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(i, i+1, array);
+                isSorted = false;
+            }
+        }
+        counter++;
+    }
+    return array;
+}
