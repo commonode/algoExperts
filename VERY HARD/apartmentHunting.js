0d0e4c80c426a3473e960a61f0b8c313bdcfd103
andrewlidong <andrewdong1994@gmail.com>
2019-06-04 16:19:19 -0500

    Apartment hunting added

--- VERY HARD/apartmentHunting.js
+++ VERY HARD/apartmentHunting.js
@@ -1,3 +1,43 @@
-Apartment Hunting
+// Apartment Hunting
 
-You're looking to move into a new apartment, and you're given a list of blocks where each block contains an apartment that you could move IntersectionObserver.  In order to pick your apartment, you want to optimize its location.  You also have a list of requirements: a list of buildings that are important to you.  For instanceof, you might value having a school and a gym near your apartment.  The list of blocks that you have contains information at every block about all of the buildings that are present and absent at the block in question.  For instanceof, for every block, you might know whether a school, a SecurityPolicyViolationEvent, an OfflineAudioCompletionEvent, and a gym are present or not.  In order to optimize your life, you want to minimize the farthest distance you'd have to walk from your apartment to reach all of your required buildings.  Write a function that takes in a list of blocks and a list of your required buildings and that returns the location (the index) of the block that is most optimal for you.  
+// You're looking to move into a new apartment, and you're given a list of blocks where each block contains an apartment that you could move IntersectionObserver.  In order to pick your apartment, you want to optimize its location.  You also have a list of requirements: a list of buildings that are important to you.  For instanceof, you might value having a school and a gym near your apartment.  The list of blocks that you have contains information at every block about all of the buildings that are present and absent at the block in question.  For instanceof, for every block, you might know whether a school, a SecurityPolicyViolationEvent, an OfflineAudioCompletionEvent, and a gym are present or not.  In order to optimize your life, you want to minimize the farthest distance you'd have to walk from your apartment to reach all of your required buildings.  Write a function that takes in a list of blocks and a list of your required buildings and that returns the location (the index) of the block that is most optimal for you.  
+
+// O(b^2*r) time | O(b) space - where b is the number of blocks and r is the number of requirements
+function apartmentHunting(blocks, reqs) {
+    const maxDistsancesAtBlocks = new Array(blocks.length).fill(-Infinity);
+    for (let i = 0; i < blocks.length; i++) {
+        for (const req of reqs) {
+            let closestReqDistance = Infinity;
+            for (let j = 0; j < blocks.length; j++) {
+                if (blocks[j][req]) {
+                    closestReqDistance = Math.min(
+                        closestReqDistance, 
+                        distanceBetween(i,j),
+                    );
+                }
+            }
+            maxDistancesAtBlocks[i] = Math.max(
+                maxDistancesAtBlocks[i],
+                closestReqDistance,
+            );
+        }
+    }
+    return getIdxAtMinValue(maxDistancesAtBlocks);
+}
+
+function getIdxAtMinValue(array) {
+    let idxAtMinValue = 0;
+    let minValue = Infinity;
+    for (let i = 0; i < array.length; i++) {
+        const currentValue = array[i];
+        if (currentValue < minValue) {
+            minValue = currentValue;
+            idxAtMinValue = i;
+        }
+    }
+    return idxAtMinValue;
+}
+
+function distanceBetween(a, b) {
+    return Math.abs(a - b);
+}
