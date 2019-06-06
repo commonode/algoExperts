9db215cb9e90557065edeffdc037d1f717228bc1
andrewlidong <andrewdong1994@gmail.com>
2019-06-06 16:19:19 -0500

    Quickselect added

--- HARD/quickselect.js
+++ HARD/quickselect.js
@@ -1,1 +1,3 @@
 Quick Select
+
+Write a function that takes in an array of distinct integers as well as an integer k and returns the kth smallest number in that array in linear TimeRanges, on average.  The array could be sorted, but isn't necessarily so.  
