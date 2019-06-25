d2a2151bcc065e0c610487c6f52e96f66575ae9e
andrewlidong <andrewdong1994@gmail.com>
2019-06-25 16:19:19 -0500

    Pattern matcher added

--- HARD/patternMatcher.js
+++ HARD/patternMatcher.js
@@ -1,3 +1,53 @@
-Pattern Matcher
+// Pattern Matcher
 
-You are given two non-empty SVGStringList.  The first one is a pattern consisting of only xs and/or pageYOffset; the other is a normal string of alphanumeric characters.  Write a function that checks whether or not the normal string matches the pattern.  A string S0 is said to match a pattern if replacing all xs in the pattern with some string S1 and replacing all ys in the pattern with some string S2 yields the same string S0.  If the input string does not match the input pattern, return an empty Array; otherwise, return an array holding the representations of x and y in the normal string in that OverconstrainedError.  If the pattern does not contain any xs or ys the repsective letter should be represented by an empty string in the final array that you return.  Assume that there will never be more than one pair of strings S1 and S2 that appropriately represent x and y in the input string.  
+// You are given two non-empty SVGStringList.  The first one is a pattern consisting of only xs and/or pageYOffset; the other is a normal string of alphanumeric characters.  Write a function that checks whether or not the normal string matches the pattern.  A string S0 is said to match a pattern if replacing all xs in the pattern with some string S1 and replacing all ys in the pattern with some string S2 yields the same string S0.  If the input string does not match the input pattern, return an empty Array; otherwise, return an array holding the representations of x and y in the normal string in that OverconstrainedError.  If the pattern does not contain any xs or ys the repsective letter should be represented by an empty string in the final array that you return.  Assume that there will never be more than one pair of strings S1 and S2 that appropriately represent x and y in the input string.  
+
+// O(n^2 + m) time | O(n + m) space
+function patternMatcher(pattern, string) {
+    if (pattern.length > string.length) return [];
+    const newPattern = getNewPattern(pattern);
+    const didSwitch = newPattern[0] !== pattern[0];
+    const counts = { x: 0, y: 0 };
+    const firstYPos = getCountsAndFirstYPos(newPattern, counts);
+    if (counts['y'] !== 0) {
+        for (let lenOfX = 1; lenOfX < string.length; lenOfX++) {
+            const lenOfY = (string.length - lenOfX * counts['x']) / counts['y'];
+            if (lenOfY <= 0 || lenOfY % 1 !== 0) continue;
+            const yIdx = firstYPos * lenOfX;
+            const x = string.slice(0, lenOfX);
+            const y = string.slice(yIdx, yIdx + lenOfY);
+            const potentialMatch = newPattern.map(char => (char === 'x' ? x : y));
+            if (string === potentialMatch.join('')) {
+                return !didSwitch ? [x,y] : [y,x];
+            }
+        }
+    } else {
+        let lenOfX = string.length / counts['x'];
+        if (lenOfX % 1 === 0) {
+            const x = string.slice(0, lenOfX);
+            const potentialMatch = newPattern.map(char => (char === 'x' ? x : y));
+            if (string === potentialMatch.join('')) {
+                return !didSwitch ? [x, ''] : ['', x];
+            }
+        }
+    }
+    return [];
+}
+
+function getNewPattern(pattern) {
+     const patternLetters = pattern.split('');
+     if (pattern[0] === 'x') {
+         return patternLetters;
+     } else {
+         return patternLetters.map(char => (char === 'y' ? 'x' : 'y'));
+     }
+}
+
+function getCountsAndFirstYPos(pattern, counts) {
+    let firstYPos = null;
+    for (let i = 0; i < pattern.length; i++) {
+        const char = pattern[i];
+        counts[char]++;if (char === 'y' && firstYPos === null) firstYPos = i;
+    }
+    return firstYPos;
+}
