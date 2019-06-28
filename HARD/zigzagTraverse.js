afb8fb988c3fff9f71ca9696ac306a8bebbe4f3d
andrewlidong <andrewdong1994@gmail.com>
2019-06-28 16:19:19 -0500

    Zigzag traverse updated

--- HARD/zigzagTraverse.js
+++ HARD/zigzagTraverse.js
@@ -1,3 +1,42 @@
-Zigzag Traverse
+// Zigzag Traverse
 
-Write a function that takes in a two dimensional array and returns a one-dimensional array of all the array's elements in zigzag order.  Zigzag starts at the top left corner of the two-dimensional array, goes down by one element, and proceeds in a zigzag pattern all the way to the bottom right corner.  
+// Write a function that takes in a two dimensional array and returns a one-dimensional array of all the array's elements in zigzag order.  Zigzag starts at the top left corner of the two-dimensional array, goes down by one element, and proceeds in a zigzag pattern all the way to the bottom right corner.  
+
+// O(n) time | O(1) space - where n is the total number of elements in the two-dimensional array
+function zigzagTraverse(array) {
+    const height = array.length - 1;
+    const width = array[0].length - 1;
+    const result = [];
+    let row = 0;
+    let col = 0;
+    let goingDown = true;
+    while (!isOutOfBounds(row, col, height, width)) {
+        result.push(array[row][col]);
+        if (goingDown) {
+            if (col === 0 || row === height) {
+                goingDown = false;
+                if (row === height) {
+                    col++;
+                } else {
+                    row++;
+                }
+            } else {
+                row++;
+                col--;
+            }
+        } else {
+            if (row === 0 || col === width) {
+                goingDown = true;
+                if (col === width) {
+                    row++;
+                } else {
+                    col++;
+                }
+            } else {
+                row--;
+                col++;
+            }
+        }
+    }
+    return result;
+}
