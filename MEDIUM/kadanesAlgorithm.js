9b4df32556009aa15a58c47c7eb852b5bee0590a
Andrew Dong <andrewdong1994@gmail.com>
2019-07-01 10:42:40 -0400

    Kadanes algorithm finished

--- MEDIUM/kadanesAlgorithm.js
+++ MEDIUM/kadanesAlgorithm.js
@@ -2,3 +2,13 @@
 
 // Write a function that takes in a non - empty array of integers and returns the maximum sum that can be obtained by summing up all the numbers in a non - empty subarray of the input array. 
 
+function kadanesAlgorithm(array) {
+    let maxEndingHere = array[0];
+    let maxSoFar = array[0];
+    for (let i = 1; i < array.length; i++) {
+        const num = array[i];
+        maxEndingHere = Math.max(num, maxEndingHere + num);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+    return maxSoFar;
+}
