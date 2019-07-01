48d8f15f78742a8d5c40f9591c3009399fba0d66
Andrew Dong <andrewdong1994@gmail.com>
2019-07-01 10:35:02 -0400

    Kadane's Algorithm updated

--- MEDIUM/kadanesAlgorithm.js
+++ MEDIUM/kadanesAlgorithm.js
@@ -1,1 +1,4 @@
-Kadanes Algorithm
+// Kadanes Algorithm
+
+// Write a function that takes in a non - empty array of integers and returns the maximum sum that can be obtained by summing up all the numbers in a non - empty subarray of the input array. 
+
