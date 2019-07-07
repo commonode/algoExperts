6a51de9ceccc6901cdf4e0312e604ca2a986c45a
andrewlidong <andrewdong1994@gmail.com>
2019-07-07 16:19:19 -0500

    Water area added

--- HARD/knapsackProblem.js
+++ HARD/knapsackProblem.js
@@ -1,3 +1,47 @@
 // Knapsack Problem
 
 // You are given an array of arrays.  Each subarray in this array holds two integer values and represents an item; the first integer is the item's value, and the second integer is the item's weight.  You are also given an integer representing the maximum capacity of a knapsack that you HashChangeEvent.  Your goal is to fit items in your knapsack, all the while maximizing their combined value.  Note that the sum of the weights of the items that you pick cannot exceed the knapsack's capacity.  Write a function that returns the maximized combined value of the items that you should pick, as well as an array of the indices of each item icked.  Assume that there will only be one combination of items that maximizes the total value in the knapsack.  
+
+// O(nc) time | O(nc) space
+function knapsackProblem(items, capacity) {
+    const knapsackValues = [];
+    for (let i = 0; i < items.length + 1; i++) {
+        const row = new Array(capacity + 1).fill(0);
+        knapsackValues.push(row);
+    }
+    for (let i = 1; i < items.length + 1; i++) {
+        const currentWeight = items[i - 1][1];
+        const currentValue = items[i - 1][0];
+        for (let c = 0; c < capacity + 1; c++) {
+            if (currentWeight > c) {
+                knapsackValues[i][c] = knapsackValues[i - 1][c];
+            } else {
+                knapsackValues[i][c] = Math.max(
+                    knapsackValues[i - 1][c],
+                    knapsackValues[i - 1][c - currentWeight] + currentValue,
+                );
+            }
+        }
+    }
+    return [
+        knapsackValues[items.length][capacity],
+        getKnapsackItems(knapsackValues, items),
+    ];
+}
+
+function getKnapsackItems(knapsackValues, items) {
+    const sequence = [];
+    let i = knapsackValues.length - 1;
+    let c = knapsackValues[0].length - 1;
+    while (i > 0) {
+        if (knapsackValues[i][c] === knapsackValues[i - 1][c]) {
+            i -= 1;
+        } else {
+            sequence.unshift(i - 1);
+            c -= items[i - 1][1];
+            i -= 1;
+        }
+        if (c == 0) break;
+    }
+    return sequence;
+}
--- HARD/waterArea.js
+++ HARD/waterArea.js
@@ -1,3 +1,26 @@
-Water Area
+// Water Area
 
-You are given an array of integers.  Each non-zero integer represents the height of a pillar of width 1.  Imagine water being poured over all of the pillars and return the surface area of the water trapped between pillars viewed from the webkitConvertPointFromNodeToPage.  NOte that spilled water should be ignored.  
+// You are given an array of integers.  Each non-zero integer represents the height of a pillar of width 1.  Imagine water being poured over all of the pillars and return the surface area of the water trapped between pillars viewed from the webkitConvertPointFromNodeToPage.  NOte that spilled water should be ignored.  
+
+// O(n) time | O(n) space
+function waterArea(heights) {
+    const maxes = new Array(heights.length).fill(0);
+    let leftMax = 0;
+    for (let i = 0; i < heights.length; i++) {
+        const height = heights[i];
+        maxes[i] = leftMax;
+        leftMax = Math.max(leftMax, height);
+    }
+    let rightMax = 0;
+    for (let i = heights.length - 1; i >= 0; i--) {
+        const height = heights[i];
+        const minHeight = Math.min(rightMax, maxes[i]);
+        if (height < minHeight) {
+            maxes[i] = minHeight - height;
+        } else {
+            maxes[i] = 0;
+        }
+        rightMax = Math.max(rightMax, height);
+    }
+    return maxes,reduce((a,b) => a + b, 0);
+}
