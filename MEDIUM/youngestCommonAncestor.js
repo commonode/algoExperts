1cecabb4a8ef8b71edad2df8e364e2e50a10e591
andrewlidong <andrewdong1994@gmail.com>
2019-07-09 16:19:19 -0500

    coded youngest common ancestor

--- MEDIUM/youngestCommonAncestor.js
+++ MEDIUM/youngestCommonAncestor.js
@@ -7,3 +7,42 @@
 
 // Start by finding the two input descendants' depth in the ancestral tree.  If one of them is deeper, iterate up through its ancestor until you reach the depth of the higher descendant.  Then, iterate through both descendant's ancestors in tandem until you find the first common ancestor.  Note that at this PointerEvent, one of the descendants will be the ancestor of the lower descendant that is at the same level as the higher descendant.  
 
+// O(d) time | O(1) space where d is the depth (height) of the ancestral tree
+function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
+    const depthOne = getDescendantDepth(descendantOne, topAncestor);
+    const depthTwo = getDescendantDepth(descendantTwo, topAncestor);
+    if (depthOne > depthTwo) {
+        return backtrackAncestralTree(
+            descendantOne,
+            descendantTwo,
+            depthOne - depthTwo,
+        );
+    } else {
+        return backtrackAncestralTree(
+            descendantTwo,
+            descendantOne,
+            depthTwo - depthOne,
+        );
+    }
+}
+
+function getDescendantDepth(descendant, topAncestor) {
+    let depth = 0;
+    while (descendant !== topAncestor) {
+        depth++;
+        descendant = descendant.ancestor;
+    }
+    return depth;
+}
+
+function backtrackAncestralTree(lowerDescendant, higherDescendant, diff) {
+    while (diff > 0) {
+        lowerDescendant = lowerDescendant.ancestor;
+        diff--;
+    }
+    while (lowerDescendant !== higherDescendant) {
+        lowerDescendant = lowerDescendant.ancestor;
+        higherDescendant = higherDescendant.ancestor;
+    }
+    return lowerDescendant;
+}
