c181ccde5a8fbdaa7adeb69e4e1f2dfe90b76add
andrewlidong <andrewdong1994@gmail.com>
2019-07-13 10:19:19 -0500

    Added youngest common ancestor

--- MEDIUM/youngestCommonAncestor.js
+++ MEDIUM/youngestCommonAncestor.js
@@ -1,1 +1,3 @@
-Youngest Common Ancestor
+// Youngest Common Ancestor
+
+// You're given three inputs, all of which are instances of a class that have an "ancestor" property pointing to their youngest ancestor.  The first input is the top ancestor in an ancestral tree (ie. the only instance that has no ancestor), and the other two inputs are descendants in the ancestral tree.  Write a function that returns the youngest common ancestor to the two descendants.  
