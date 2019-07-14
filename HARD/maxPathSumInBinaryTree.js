0f5aea3d72b27596e02292c79413a3a13e027034
andrewlidong <andrewdong1994@gmail.com>
2019-07-14 10:19:19 -0500

    Max path sum in binary tree added

--- HARD/maxPathSumInBinaryTree.js
+++ HARD/maxPathSumInBinaryTree.js
@@ -1,1 +1,3 @@
 Max Path Sum in Binary Tree
+
+Write a function that takes in a Binary Tree and returns its max path sum.  A path is a collection of connected nodes where no node is connected to more than two other nodes; a path sum is the sum of the values of the nodes in a particular path.  Each binary tree node has a value stored in a property called "value" and two children nodes stored in properties called 'left' and 'right' respectively.  Children nodes can either be Binary Tree nodes themselves of the None(null) value.  
