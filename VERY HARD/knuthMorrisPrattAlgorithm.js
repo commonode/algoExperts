919cba014eaec5e055c0cfef9d90591ff5dd3f02
andrewlidong <andrewdong1994@gmail.com>
2019-07-25 16:19:19 -0500

    Added knuth morris pratt algorithm

--- VERY HARD/knuthMorrisPrattAlgorithm.js
+++ VERY HARD/knuthMorrisPrattAlgorithm.js
@@ -1,1 +1,3 @@
 Knuth-Morris-Pratt algorithm
+
+Write a function that takes in two strings and checks if the first string contains the second one using the Knuth-Morris-Pratt algorithm.  The function should return a Boolean.  
