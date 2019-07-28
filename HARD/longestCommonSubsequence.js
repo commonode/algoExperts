ca8f72ffd47c3d134f1156b7cbd521daf7425752
andrewlidong <andrewdong1994@gmail.com>
2019-07-28 16:19:19 -0500

    Added longest common subsequence

--- HARD/longestCommonSubsequence.js
+++ HARD/longestCommonSubsequence.js
@@ -1,1 +1,3 @@
 Longest Common Subsequence
+
+Implement a function that returns the longest subsequence common to two given SVGStringList.  A subsequence is defined as a gorup of characters that appear sequentially, with no importance given to their actual position in a string.  In other words, characters do not need to appear consecutively in order to form a subsequence.  Assume that there will only be one longest common subsequence.  
