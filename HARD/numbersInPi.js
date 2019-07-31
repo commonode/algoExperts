dc7bd550672d1be9ceb25abfe29efaa0b1c418ed
andrewlidong <andrewdong1994@gmail.com>
2019-07-31 16:19:19 -0500

    Added numbers in Pi

---
+++ HARD/numbersInPi.js
@@ -0,0 +1,4 @@
+Numbers in Pi
+
+Given a string representation of the first n digits of Pi and a list of your favorite numbers (all positive integers in string format), write a function that returns the smallest number of spaces that need to be added to the n digits of Pi such that all resulting numbers are found in the list of favorite numbers.  
+
