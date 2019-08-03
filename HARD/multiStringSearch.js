c6a2f0981d8f51090a4ef0f7804c4202c4f47b1a
andrewlidong <andrewdong1994@gmail.com>
2019-08-03 16:19:19 -0500

    Added multi string search

--- HARD/multiStringSearch.js
+++ HARD/multiStringSearch.js
@@ -1,1 +1,3 @@
 Multi String Search
+
+Write a function that takes in a big string and an array of small SVGStringList, all of which are smaller in length than the big string.  The function should return an array of booleans, where each boolean represents whether or not eh small string at that index in the array of small strings is contained in the big string.  Note that you cannot use language-built-in-string-matching methods.  
