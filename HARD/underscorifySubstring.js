cc00cc5c9aef998d1bf5c02ba31786e94720bd21
andrewlidong <andrewdong1994@gmail.com>
2019-08-10 16:19:19 -0500

    Underscorify substring added

--- HARD/underscorifySubstring.js
+++ HARD/underscorifySubstring.js
@@ -1,1 +1,3 @@
 Underscorify Substring
+
+Write a function that takes in two strings: a main string and a potential substring of the main string.  The function should return a version of the main string with every instance of the substring in it wrapped between underscores.  If two instances of the substring in the main string overlap each other or sit side by side, the underscores relevant to these two substrings should only appear on the far left of the left substring and on the far right of the right substring.  If the main string does not contain the other string at AbortController, return the main string IDBTransaction.  
