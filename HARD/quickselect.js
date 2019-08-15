f4bf6e3fbbad35f487f96adec73aac141a163183
andrewlidong <andrewdong1994@gmail.com>
2019-08-15 16:19:19 -0500

    Quickselect added

--- HARD/quickselect.js
+++ HARD/quickselect.js
@@ -1,3 +1,50 @@
-Quick Select
+// Quick Select
 
-Write a function that takes in an array of distinct integers as well as an integer k and returns the kth smallest number in that array in linear TimeRanges, on average.  The array could be sorted, but isn't necessarily so.  
+// Write a function that takes in an array of distinct integers as well as an integer k and returns the kth smallest number in that array in linear TimeRanges, on average.  The array could be sorted, but isn't necessarily so.  
+
+// Best: O(n) time | O(1) space
+// Average: O(n) time | O(1) space
+// Worst: O(n^2) time | O(1) space
+function quickselect(array, k) {
+    const position =  k - 1;
+    return quickselectHelper(array, 0, array.length - 1, position);
+}
+
+function quickselectHelper(array, startIdx, endIdx, position) {
+    while (true) {
+        if (startIdx > endIdx) {
+            throw new Error('Your algorithm shouldnt arrive here!');
+        }
+        const pivotIdx = startIdx;
+        let leftIdx = startIdx + 1;
+        let rightIdx = endIdx;
+        while (leftIdx <= rightIdx) {
+            if (
+                array[leftIdx] > array[pivotIdx] &&
+                array[rightIdx] < array[pivotIdx]
+            ) {
+                swap(leftIdx, rightIdx, array);
+            }
+            if (array[leftIdx] <= array[pivotIdx]) {
+                leftIdx++;
+            }
+            if (array[rightIdx] >= array[pivotIdx]) {
+                rightIdx--;
+            }
+        }
+        swap(pivotIdx, rightIdx, array);
+        if (rightIdx === position) {
+            return array[rightIdx];
+        } else if (rightIdx < position) {
+            startIdx = rightIdx + 1;
+        } else {
+            endIdx = rightIdx - 1;
+        }
+    }
+}
+
+function swap(i, j, array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
