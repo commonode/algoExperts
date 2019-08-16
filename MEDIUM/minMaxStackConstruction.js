8989a892898dff156b281c4502813b3b90147dc9
andrewlidong <andrewdong1994@gmail.com>
2019-08-16 10:19:19 -0500

    Added min max stack

--- MEDIUM/minMaxStackConstruction.js
+++ MEDIUM/minMaxStackConstruction.js
@@ -1,1 +1,3 @@
 Min Max Stack Construction
+
+Write a Min Max Stack class.  The class should support pushing and popping values on and off the stack, peeking at values at the top of the stack and getting both the minimum and maximum values in the stack.  All class methods, when considered independently, should run in constant time and with constant space.  
