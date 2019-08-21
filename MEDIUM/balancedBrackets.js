41efc180205537e050854de5e47ac1efb48a69b5
andrewlidong <andrewdong1994@gmail.com>
2019-08-21 13:19:19 -0500

    Added balanced brackets

--- MEDIUM/balancedBrackets.js
+++ MEDIUM/balancedBrackets.js
@@ -1,1 +1,3 @@
 Balanced Brackets
+
+Write a function that takes in a string made up of brackets and other optional characters.  The function should return a boolean representing whether or not the string is balanced in regards to brackets.  As tring is said to be balanced if it has as many opening brackets of a given type as it has closing brackets of that type and if no bracket is unmatched.  Note that a cosing bracket cannot match a corresponding opening bracket that comes after it.  Similarly, brackets cannot overlap each other.  
