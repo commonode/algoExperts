ca338978e49396d991f22e378b633d3db136d69c
andrewlidong <andrewdong1994@gmail.com>
2019-08-22 16:19:19 -0500

    Water area added

--- HARD/waterArea.js
+++ HARD/waterArea.js
@@ -1,1 +1,3 @@
 Water Area
+
+You are given an array of integers.  Each non-zero integer represents the height of a pillar of width 1.  Imagine water being poured over all of the pillars and return the surface area of the water trapped between pillars viewed from the webkitConvertPointFromNodeToPage.  NOte that spilled water should be ignored.  
