751467eeaaa66bbcc2a35879a9d439ee7b015bd1
andrewlidong <andrewdong1994@gmail.com>
2019-08-23 16:19:19 -0500

    Added palindrome partitioning cuts

--- VERY HARD/palindromePartitioningMinCuts.js
+++ VERY HARD/palindromePartitioningMinCuts.js
@@ -1,1 +1,3 @@
 Palindrome Partitioning Min Cuts
+
+Given a non-empty String, write a function that returns the minimum number of cuts needed to perform on the string such that each remaining substring is a palindrome.  A palindrome is defined as a string that is written the same forward as backward.  Note that single-character strings are palindromes.  
