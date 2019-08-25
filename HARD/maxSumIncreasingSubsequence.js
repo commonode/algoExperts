8110c10f317047d3ec149b6a7a0a4612bee05f75
andrewlidong <andrewdong1994@gmail.com>
2019-08-25 16:19:19 -0500

    Added max sum increasing subsequence

--- HARD/maxSumIncreasingSubsequence.js
+++ HARD/maxSumIncreasingSubsequence.js
@@ -1,1 +1,3 @@
 Max Sum Increasing Subsequence
+
+Given an non-empty array of integers, write a function that returns an array of length 2.  The first element in the output array should be an intger value representing the greatest sum that can be generated from a strictly-increasing subsequence in the array.  The second element should be an array of the numbers in that subsequence.  A subsequence is defined as a set of numbers that are not necessarily adjacent but that are in the same order as they appear in the array.  Assume that there will only be one increasing subsequence with the greatest sum.  
