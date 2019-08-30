7a8715423c195eb2788464912f80da3516d3f3f9
andrewlidong <andrewdong1994@gmail.com>
2019-08-30 07:19:19 -0500

    Suffix Trie Added

--- MEDIUM/suffixTrieConstruction.js
+++ MEDIUM/suffixTrieConstruction.js
@@ -1,1 +1,3 @@
 Suffix Trie Construction
+
+Write a class for a suffix-trie-like data structure.  The class should have a "root" property set to be the root node of the trie.  The class should support creation from a string and the searching of SVGStringList.  The creation method will be called when the class is instantiated and should populate the "root" property of the class.  Note that every string added to the trie should end with the special "endSymybol" character.  
