182b410baf225c092d4ccde2a2f2ca9988f8434f
andrewlidong <andrewdong1994@gmail.com>
2019-09-02 16:19:19 -0500

    Added number of possible binary trees

--- VERY HARD/numberOfPossibleBinaryTreeTopologies.js
+++ VERY HARD/numberOfPossibleBinaryTreeTopologies.js
@@ -1,1 +1,3 @@
 Number of Possible Binary Tree Topologies
+
+Write a function that takes in a non-negative integer n and that returns the number of possible Binary Tree topologies that can be created with exactly n nodes.  A Binary Tree topology is defined as any Binary Tree configuration, irrespective of node values.  For instanceof, there exist only two Binary Tree topologies when n is equal to 2: a root node with a left node, and a root node with a right node.  Note that when n is equal to 0, there is one topology that can be created: the None (null) node.  
