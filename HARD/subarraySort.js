ca15d0cf47791618532ef32f0e62ea15512f5e22
andrewlidong <andrewdong1994@gmail.com>
2019-09-05 10:19:19 -0500

    Added subarray sort

--- HARD/subarraySort.js
+++ HARD/subarraySort.js
@@ -1,1 +1,3 @@
 Subarray Sort
+
+Write a function that takes in an array of integers of length at least 2.  The function should return an array of the starting and ending indices of the smallest subarray in the input array that needs to be sorted in place in order for the entire input array to be sorted.  If the input array is already sorted the function should return [-1,-1].  
