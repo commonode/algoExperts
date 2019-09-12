f0757529fc6ab596d9982bbec6b654bf579d9b02
andrewlidong <andrewdong1994@gmail.com>
2019-09-12 16:19:19 -0500

    Added lowest common manager

--- HARD/lowestCommonManager.js
+++ HARD/lowestCommonManager.js
@@ -1,1 +1,3 @@
 Lowest Common Manager
+
+You're given three inputs, all of whicha re instances of a class that have a "directReports" property pointing to their direct reports.  The first input is the top manager in an organizational chart (i.e. the only instance that is not anybody else's direct report), and the other two inputs are reports in the organizational chart.  Write a function that returns the lowest common manager to the two reports.  
