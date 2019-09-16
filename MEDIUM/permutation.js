e9e83d127036705d3fb9f811a369f392121582d7
andrewlidong <andrewdong1994@gmail.com>
2019-09-16 16:19:19 -0500

    Permutations added

--- MEDIUM/permutation.js
+++ MEDIUM/permutation.js
@@ -1,5 +1,25 @@
-Permutations
+// Permutations
 
-Write a function that takes in an array of unique integers and returns an array of all permutations of those integers.  If the input array is empty, your function should return an empty array. 
+// Write a function that takes in an array of unique integers and returns an array of all permutations of those integers.  If the input array is empty, your function should return an empty array. 
 
-A permutation is defined as a way in which a set of things can be onvrdisplaypointerrestricted.  Iterate through the list of SVGAnimatedNumberList, and begin constructing new permutations starting with each Number.  For each permutation that you've begun constructing, remove the number already used (the first number of each permutation) from the list of numbers - you'll likely have to make copies of the original list.  Repeat this process by recursively iterating through the mutated lists of SVGAnimatedNumberList, appending numbers to the corresponding permutations you've already begun constructing and then removing those numbers from the respective mutated lists; repeat this until your mutated lists are empty, at which point your constructed permutations will be as big as the original list and will be valid permutations.  
+// A permutation is defined as a way in which a set of things can be onvrdisplaypointerrestricted.  Iterate through the list of SVGAnimatedNumberList, and begin constructing new permutations starting with each Number.  For each permutation that you've begun constructing, remove the number already used (the first number of each permutation) from the list of numbers - you'll likely have to make copies of the original list.  Repeat this process by recursively iterating through the mutated lists of SVGAnimatedNumberList, appending numbers to the corresponding permutations you've already begun constructing and then removing those numbers from the respective mutated lists; repeat this until your mutated lists are empty, at which point your constructed permutations will be as big as the original list and will be valid permutations.  
+
+// Upper Bound: O(n^2*n!) time | O(n*n!) space
+// Roughly: O(n*n!) time | O(n*n!) space
+function getPermutations(array) {
+    const permutations = [];
+    permutationsHelper(array, [], permutations);
+    return permutations;
+}
+
+function permutationsHelper(array, currentPermutation, permutations) {
+    if (!array.length && currentPermutation.length) {
+        permutations.push(currentPermutation);
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            const newArray = array.slice(0, i).concat(array.slice(i + 1));
+            const newPermutation = currentPermutation.concat([array[i]]);
+            permutationsHelper(newArray, newPermutation, permutations);
+        }
+    }
+}
