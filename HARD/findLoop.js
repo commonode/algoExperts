9e3e2874e5b484735b5a8812f0f460bde6bc95b3
andrewlidong <andrewdong1994@gmail.com>
2019-09-22 16:19:19 -0500

    Find loop added

--- HARD/findLoop.js
+++ HARD/findLoop.js
@@ -1,1 +1,3 @@
 Find Loop
+
+Write a function that takes in the head of a singly linked list that contains a loop (in other words, the list's tail node points to some node in the list instead of the None(null) value).  The function should return the node (the actual node - not just its vaule) from which the loop originates in constant space.  Note that every node in the Singly Linked List has a "value" property storing its value as well as a "next" property pointing to the next node in the list.  
