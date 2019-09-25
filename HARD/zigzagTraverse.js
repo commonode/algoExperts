97487420bcbb4ad7e01ad4192474555f8022d170
andrewlidong <andrewdong1994@gmail.com>
2019-09-25 08:19:19 -0500

    Added zigzag traverse

---
+++ HARD/zigzagTraverse.js
@@ -0,0 +1,3 @@
+Zigzag Traverse
+
+Write a function that takes in a two dimensional array and returns a one-dimensional array of all the array's elements in zigzag order.  Zigzag starts at the top left corner of the two-dimensional array, goes down by one element, and proceeds in a zigzag pattern all the way to the bottom right corner.  
