bf0dd48cc3a6e4240817fbe2e0a831847d7d8884
andrewlidong <andrewdong1994@gmail.com>
2019-10-07 19:24:37 -0400

    Updated binary search

--- EASY/binarySearch.js
+++ EASY/binarySearch.js
@@ -1,7 +1,7 @@
 // Binary Search 
 
 // Write a function that takes in a sorted array of integers as well as a target integer.  
-
+// 
 // The function should use the Binary Search algorthm to find if the target number is contained in the array and should return its index if it is, otherwise -1.  
 
 function binarySearch(array, target) {
