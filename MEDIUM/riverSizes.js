f30d6e98ab48c88ef3962f7933783b48e757c519
andrewlidong <andrewdong1994@gmail.com>
2019-10-15 17:25:15 -0400

    Initial attempt at river sizes

--- MEDIUM/riverSizes.js
+++ MEDIUM/riverSizes.js
@@ -1,1 +1,44 @@
-River Sizes
+// River Sizes
+
+// You are given a two dimensional array(matrix) of potentially unequal height and width containing only 0s and 1s.Each 0 represents land and each 1 represents a part of a river.Write a function that returns an array of the sizes of all rivers represented in the input matrix.  
+
+// O(wh) time | O(wh) space
+function riverSizes(matrix) {
+    const sizes = [];
+    const visited = matrix.map(row => row.map(value => false));
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix.length; j++) {
+            if (visited[i][j]) continue;
+            traverseNode(i,j,matrix, visited, sizes);
+        }
+    }
+    return sizes;
+}
+
+function traverseNode(i, j, matrix, visited, sizes) {
+    let currentRiverSize = 0;
+    const nodesToExplore = [[i,j]];
+    while (nodesToExplore.length) {
+        const currentNode = nodesToExplore.pop();
+        i = currentNode[0];
+        j = currentNode[1];
+        if (visited[i][j]) continue;
+        visited[i][j] = true;
+        if (matrix[i][j] === 0) continue;
+        currentRiverSize++;
+        const unvisitedNeighbors = getUnvisitedNeighbors(i, j, matrix, visited);
+        for (const neighbor of unvisitedNeighbors) {
+            nodesToExplore.push(neighbor);
+        }
+    }
+    if (currentRiverSizes > 0) sizes.push(currentRiverSize);
+}
+
+function getUnvisitedNeighbors(i, j, matrix, visited) {
+    const unvisitedNeighbors = [];
+    if (i > 0 && !visited[i-1][j]) unvisitedNeighbors.push([i - 1, j]);
+    if (i < matrix.length - 1 && !visited[i + 1][k]) unvisitedNeighbors.push([i + 1, j]);
+    if (j > 0 && !visited[i][j - 1]) unvisitedNeighbors.push([i, j - 1]);
+    if (j < matrix[0].length - 1 && !visited[i][j + 1]) unvisitedNeighbors.push([i, j + 1]);
+    return unvisitedNeighbors;
+}
