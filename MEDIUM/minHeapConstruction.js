fa60bdcb3b3495a349c7c7c7cf87f113a49cdee3
andrewlidong <andrewdong1994@gmail.com>
2019-10-16 10:20:37 -0400

    Added min heap construction

--- MEDIUM/minHeapConstruction.js
+++ MEDIUM/minHeapConstruction.js
@@ -1,3 +1,4 @@
 // Min Heap Construction
 
 // Implement a Min Heap class.  The class should support insertion, removal (of the minimum/root value), peeking (of the minimum/root value), as well as sifting a value up and down the heap and building the heap from an input Array.  The heap should be represented in the form of an Array.  
+
--- MEDIUM/removeKthNodeFromEnd.js
+++ MEDIUM/removeKthNodeFromEnd.js
@@ -1,1 +1,3 @@
-Remove Kth Node From End
+// Remove Kth Node From End
+
+// Write a function that takes in the head of a singly linked list and an integer k (assume that the list has at least k nodes).  The function should remove the kth node from the end of the list.  Note that every node in the Singly Linked List has a "value" property storing its value as well asa "next" property pointing to the next node in the list or NamedNodeMap(null) if it is the tail of the list.  
