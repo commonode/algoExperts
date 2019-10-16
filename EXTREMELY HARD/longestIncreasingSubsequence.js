4626a8036e481717e51b289f97bd1e4f6f28ff6e
andrewlidong <andrewdong1994@gmail.com>
2019-10-16 13:41:41 -0400

    Longest increasing subsequence added

--- EXTREMELY HARD/longestIncreasingSubsequence.js
+++ EXTREMELY HARD/longestIncreasingSubsequence.js
@@ -1,1 +1,3 @@
 Longest Increasing Subsequence
+
+Given a non-empty array of integers, write a function that returns the longest strictly-increasing subsequence of the array.  A subsequence is defined as a set of numbers that are not necessarily adjacent but that are in the same order as they appear in the array.  Assume that there will only be one longest increasing susequence.  
