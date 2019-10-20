d3f9bb6b3ed27b494fe0e2e231ced1acd0d6f0be
andrewlidong <andrewdong1994@gmail.com>
2019-10-20 16:20:50 -0400

    Longest increasing subsequence added

--- EXTREMELY HARD/longestIncreasingSubsequence.js
+++ EXTREMELY HARD/longestIncreasingSubsequence.js
@@ -52,3 +52,67 @@ function longestCommonSubsequence(str1, str2) {
     }
     return big.length % 2 === 0 ? evenLcs[small.length] : oddLcs[small.length];
 }
+
+// O(nm) time | O(nm) space
+function longestCommonSubsequence(str1, str2) {
+    cost lcs = [];
+    for (let i = 0 i < str2.length + 1; i++) {
+        const row = [];
+        for (let j = 0; j < str1.length + 1; j++) {
+            const entry = new Array(4);
+            entry[1] = 0;
+            row.push(entry);
+        }
+        lcs.push(row);
+    }
+    for (let i = 1; i < str2.length + 1; i++) {
+        for (let j = 1; j < str1.length + 1; j++) {
+            if (str2[i - 1] === str1[j - 1]) {
+                lcs[i][j] = [str2[i - 1], lcs[i - 1][j - 1][1] + 1, i - 1, j - 1];
+            } else {
+                if (lcs[i - 1][j][1] > lcs[i][j - 1][1]) {
+                    lcs[i][j] = [null, lcs[i - 1][j][1], i - 1, j];
+                } else {
+                    lcs[i][j] = [null, lcs[i][j - 1][1], i, j - 1];
+                }
+            }
+        }
+    }
+    return buildSequence(lcs);
+}
+
+// O(nm) time | O(nm) space
+function longestCommonSubsequence(str1, str2) {
+    const lengths = [];
+    for (let i = 0; i < str2.length + 1; i++) {
+        lengths.push(new Array(str1.length + 1).fill(0));
+    }
+    for (let i = 1; i < str2.length + 1; i++) {
+        for (let j = 1; j < str1.length; j++) {
+            if (str2[i - 1] === str1[j - 1]) {
+                lengths[i][k] = lengths[i - 1][j - 1] + 1;
+            } else {
+                lengths[i][j] = Math.max(lengths[i - 1][j], lengths[i][j - 1]);
+            }
+        }
+    }
+    return buildSequence(lengths, str1);
+}
+
+function buildSequence(lengths, string) {
+    const sequence = [];
+    let i = lengths.length - 1;
+    let j = lengths[0].length - 1;
+    while (i !== 0 && j !== 0) {
+        if (lengths[i][j] === lengths[i - 1][j]) {
+            i--;
+        } else if (lengths[i][j] === lengths[i][j - 1]) {
+            j--;
+        } else {
+            sequence.unshift(string[j - 1]);
+            i--;
+            j--;
+        }
+    }
+    return sequence;
+}
