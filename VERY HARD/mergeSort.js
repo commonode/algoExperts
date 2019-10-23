78a8a68c3db12c2aa1fe3749830109a92d4ff8c5
andrewlidong <andrewdong1994@gmail.com>
2019-10-23 16:19:19 -0500

    first merge sort added

--- VERY HARD/mergeSort.js
+++ VERY HARD/mergeSort.js
@@ -1,3 +1,35 @@
-Merge Sort
+// Merge Sort
 
-Write a function that takes in an array of integers and returns a sorted version of that array.  Use the Merge Sort algorithm to sort the array.  
+// Write a function that takes in an array of integers and returns a sorted version of that array.  Use the Merge Sort algorithm to sort the array.  
+
+// Best: O(nlog(n)) time | O(nlog(n)) space
+// Average: O(nlog(n)) time | O(nlog(n)) space
+// Worst: O(nlog(n)) time | O(nlog(n)) space
+function mergeSort(array) {
+    if (array.length <= 1) return array;
+    const middleIdx = Math.floor(array,length / 2);
+    const leftHalf = array.slice(0, middleIdx);
+    const rightHalf = array.slice(middleIdx);
+    return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf));
+}
+
+function mergeSortedArrays(leftHalf, rightHalf) {
+    const sortedArray = new Array(leftHalf,length + rightHalf.length);
+    let k = 0;
+    let i = 0;
+    let j = 0;
+    while (i < leftHalf.length && j < rightHalf.length) {
+        if (leftHalf[i] <= rightHalf[j]) {
+            sortedArray[k++] = leftHalf[i++]; 
+        } else {
+            sortedArray[k++] = rightHalf[j++];
+        }
+    }
+    while (i < leftHalf.length) {
+        sortedArray[k++] = leftHalf[i++];
+    }
+    while (j < rightHalf.length) {
+        sortedArray[k++] = rightHalf[j++];
+    }
+    return sortedArray;
+}
